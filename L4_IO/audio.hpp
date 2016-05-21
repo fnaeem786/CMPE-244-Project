0672e2c6cb840c37d2b87a667bb584252ad36103
fnaeem786 <fnaeem@yahoo.com>
2016-05-21 13:45:02 -0700

    Created singleton audio class for audio device interface

---
+++ L4_IO/audio.hpp
@@ -0,0 +1,37 @@
+/*
+ * audio.hpp
+ *
+ *  Created on: May 20, 2016
+ *      Author: Farhan Naeem
+ */
+
+#ifndef L4_IO_AUDIO_HPP_
+#define L4_IO_AUDIO_HPP_
+
+
+#include "singleton_template.hpp"
+#include <string.h>
+#include <storage.hpp>
+
+class audio : public SingletonTemplate<audio>
+{
+
+public:
+
+
+
+
+protected:
+	friend class SingletonTemplate<audio>;
+
+private:
+
+
+};
+
+
+#endif /* L4_IO_AUDIO_HPP_ */
+
+
+
+
